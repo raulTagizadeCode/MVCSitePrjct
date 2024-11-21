df8f22a06aaaf4df476ec8b8c67e89c097032a07
Nuran Piriyev <nuranp-ab205@code.edu.az>
2024-11-21 11:58:34 +0400

    Initial commit

---
+++ MVCproject/Views/Home/work.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVCproject.Views.Home
+{
+    public class workModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
